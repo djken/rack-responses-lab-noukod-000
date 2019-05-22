745560d7ceb455e3e992569e4daa13f5fab89e6f
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-05-22 02:38:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,12 +5,12 @@ class Application
     time = Time.now.hour
 
     # time <= 12 ? resp.write "Good Morning!" : resp.write "Good Afternoon!" resp.finish
-    while time <= 12
+    if time <= 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
     end
-
+  
     resp.finish
   end
 end
